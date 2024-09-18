069f191124062c9b5ec473b9f0225f2da8d0e6b3
marcilia.guilger@ifood.com.br <marcilia.guilger@ifood.com.br>
2024-09-18 19:36:12 -0300

    teste

---
+++ main.tf
@@ -0,0 +1,43 @@
+provider "aws" {
+  region = "us-east-1"  # Substitua pela sua região preferida
+  profile = "pos"
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "my_lambda" {
+  function_name = "myLambdaFunction"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "index.handler"
+  runtime       = "nodejs16.x"
+
+  filename      = "lambda_function.zip"  # O arquivo zip da sua função Lambda
+
+  source_code_hash = filebase64sha256("lambda_function.zip")
+
+  environment {
+    variables = {
+      foo = "bar"
+    }
+  }
+}
