b8fd1073938031489f366be0249b7a9a4c37e9f1
marcilia.guilger@ifood.com.br <marcilia.guilger@ifood.com.br>
2024-09-18 18:50:12 -0300

    teste

--- lambda.tf
+++ lambda.tf
@@ -9,3 +9,35 @@ module "lambda_function" {
     Name = "my-lambda"
   }
 }
+
+module "ts_lambda_function" {
+  source = "terraform-aws-modules/lambda/aws"
+  function_name = "ts-my-lambda"
+  description   = "My lambda function written in TS"
+  handler       = "index.handler"
+  runtime       = "nodejs18.x"
+  source_path = [{
+    path = "../src"
+    commands = [
+      "npm ci", # install dependencies
+      "npm run build", # npx tsc to transpile
+      "npm prune --production"
+      ":zip" # zip all
+    ]
+  }]
+  publish = true
+  environment_variables = {
+    ENV = "dev"
+  }
+  attach_policy_statements = true
+  policy_statements = {
+    cloud_watch = {
+      effect    = "Allow",
+      actions   = ["cloudwatch:PutMetricData"],
+      resources = ["*"]
+    }
+  }
+  tags = {
+    Name = "ts-lambda"
+  }
+}
