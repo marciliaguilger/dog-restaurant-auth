8dfab6a04bfd190377406db6c21636b2213ae700
marcilia.guilger@ifood.com.br <marcilia.guilger@ifood.com.br>
2024-09-30 19:53:12 -0300

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,22 +3,22 @@ provider "aws" {
   # profile = "pos"
 }
 
-#resource "aws_iam_role" "lambda_role" {
-#  name = "lambda-execution-role"
-#
-#  assume_role_policy = jsonencode({
-#    Version = "2012-10-17",
-#    Statement = [
-#      {
-#        Effect = "Allow",
-#        Principal = {
-#          Service = "lambda.amazonaws.com"
-#        },
-#        Action = "sts:AssumeRole"
-#      }
-#    ]
-#  })
-#}
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
 
 resource "aws_iam_role_policy_attachment" "lambda_policy" {
   role       = aws_iam_role.lambda_role.name
